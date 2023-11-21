00d386719a496c258e3536a224271c2fb132946b
anderlun <anderzito1999@gmail.com>
2023-11-21 11:47:12 -0500

    cambios

--- js/merca.js
+++ js/merca.js
@@ -1,12 +1,8 @@
-/* cuando trabajamos con objetos debo tener en cuenta que en un objeto se construye teniendo encuenta una clase*/
-// el contructor crea la clase osea la estructura
-let inputBusqueda = document.getElementById
-    ("inputBusqueda");
+let inputBusqueda = document.getElementById("inputBusqueda");
 let mainContent = document.getElementById("mainContent");
 
 class Automovil {
-    constructor(marca, modelo, precio, imagen, año, kilometro, ubicacion) {
-
+    constructor(marca, modelo, precio, imagen, año, kilometro, ubicacion, calificacion) {
         this.marca = marca;
         this.modelo = modelo;
         this.precio = precio;
@@ -14,44 +10,32 @@ class Automovil {
         this.año = año;
         this.kilometro = kilometro;
         this.ubicacion = ubicacion;
-
+        this.calificacion = calificacion;
     }
 }
 
-let auto1 = new Automovil("Toyotas", "TXL", 15000000, "images/toyota.jpg", "2021", 25, "pitalito - huila");
-let auto2 = new Automovil("Mustang", "PRO", 250000000, "images/mustang.png", "2023", 0, "porvenir - huila");
-let auto3 = new Automovil("CHEVROLET ", "CAMARO", 350000000, "images/camaro2.png", "2023", 0, "exito - huila");
-let auto4 = new Automovil("ROLL ", "ROYCE", 1300000000, "images/roll-rois.jpg", "2024", 0, "Popular - huila");
+let auto1 = new Automovil("Toyotas", "TXL", 15000000, "images/toyota.jpg", "2021", 25, "pitalito - huila", 0.2);
+let auto2 = new Automovil("Mustang", "PRO", 250000000, "images/mustang.png", "2023", 0, "porvenir - huila", 0.6);
+let auto3 = new Automovil("CHEVROLET ", "CAMARO", 350000000, "images/camaro2.png", "2023", 0, "exito - huila", 1.4);
+let auto4 = new Automovil("ROLL ", "ROYCE", 1300000000, "images/roll-rois.jpg", "2024", 0, "Popular - huila", 2.5);
 
-let autos = [
-    auto1, auto2, auto3,auto4
-]
-
+let autos = [auto1, auto2, auto3, auto4];
 
 window.addEventListener("load", function () {
-
     mostrarProducto('');
-
     inputBusqueda.addEventListener('input', function () {
         mostrarProducto(inputBusqueda.value);
     });
 });
 
 function mostrarProducto(terminoBusqueda) {
-
     mainContent.innerHTML = '';
     autos.forEach(auto => {
         if (auto.marca.toLowerCase().includes(terminoBusqueda.toLowerCase()) || auto.modelo.toLowerCase().includes(terminoBusqueda.toLowerCase())) {
-
-            //alert(auto1.marca+ " "+auto1.modelo);
-
-
             let boxproducto = document.createElement("div");
             mainContent.appendChild(boxproducto);
             boxproducto.setAttribute("class", "box-producto");
 
-
-            /*caja para la imagen*/
             let boximg = document.createElement("div");
             boxproducto.appendChild(boximg);
             boximg.setAttribute("class", "box-img");
@@ -60,8 +44,6 @@ function mostrarProducto(terminoBusqueda) {
             imgAuto.setAttribute("src", auto.imagen);
             imgAuto.setAttribute("class", "img-auto");
 
-
-            //caja información
             let boxInfo = document.createElement("div");
             boxproducto.appendChild(boxInfo);
             boxInfo.setAttribute("class", "box-info");
@@ -72,23 +54,14 @@ function mostrarProducto(terminoBusqueda) {
             marcaL.appendChild(txtNodeMarca);
             marcaL.setAttribute("class", "marca");
 
-
-            /*precio*/
             let precio = document.createElement("label");
             boxInfo.appendChild(precio);
 
-
-            // Formatea el precio utilizando toLocaleString()
             let precioFormateado = auto.precio.toLocaleString();
-
             let txtNodePrecio = document.createTextNode("$" + precioFormateado);
             precio.appendChild(txtNodePrecio);
             precio.setAttribute("class", "precio");
 
-
-
-
-            /* Información adicional */
             let infoAdicional = document.createElement("label");
             boxInfo.appendChild(infoAdicional);
 
@@ -96,7 +69,6 @@ function mostrarProducto(terminoBusqueda) {
             infoAdicional.appendChild(infoText);
             infoAdicional.setAttribute("class", "info-adicional");
 
-            /*icono corazon*/
             let boxCorazon = document.createElement("div");
             boximg.appendChild(boxCorazon)
 
@@ -106,16 +78,55 @@ function mostrarProducto(terminoBusqueda) {
             boxCorazon.setAttribute("class", "box-corazon");
             iconoCorazon.setAttribute("class", "icon-corazon fa-regular fa-heart");
 
-            /*linea divisora*/
-
             let lineaDiv = document.createElement("hr");
             boxproducto.appendChild(lineaDiv);
             lineaDiv.setAttribute("class", "linea-div");
+
+            // Campo de entrada para la calificación
+            let inputCalificacion = document.createElement("input");
+            boxInfo.appendChild(inputCalificacion);
+            inputCalificacion.setAttribute("type", "number");
+            inputCalificacion.setAttribute("placeholder", "Ingrese la calificación");
+            inputCalificacion.addEventListener("input", function () {
+                mostrarCalificacion(inputCalificacion.value, boxCalificacion);
+            });
+
+            // Contenedor para mostrar las estrellas de calificación
+            let boxCalificacion = document.createElement("div");
+            boxInfo.appendChild(boxCalificacion);
+            boxCalificacion.setAttribute("class", "calificacion-container");
+
+            // Inicializar la calificación al cargar la página
+            mostrarCalificacion(inputCalificacion.value, boxCalificacion);
         }
     });
+}
+
+function mostrarCalificacion(calificacion, container) {
+    // Limpiar el contenido actual del contenedor
+    container.innerHTML = '';
+
+    // Convertir la calificación a un número de rango 0 a 10
+    let numEstrellas = Math.min(Math.max(calificacion * 2, 0), 10); // Ajusta la calificación a un rango válido (0-10)
+
+    // Crear estrellas según el rango
+    for (let i = 0; i < 5; i++) {
+        let estrella = document.createElement("i");
+        container.appendChild(estrella);
+        estrella.setAttribute("class", "icon-estrella fa-solid fa-star");
+
+        // Verificar si la estrella actual debe estar activa, a medias o inactiva
+        if (i * 2 + 1 <= numEstrellas) {
+            if (numEstrellas - i * 2 >= 1) {
+                estrella.classList.add("activa");
+            } else {
+                estrella.classList.add("media");
+            }
+        }
+    }
+}
 
 
-};
 
 window.addEventListener("load", function () {
     mostrarProducto('');
